41b3d571c5c34c6c63f499df8c3076eb5be6abcc
Arjan <github@arjanonline.net>
2016-03-26 00:27:16 +0100

    Displays sunrise and sunset times after current position has been determined.

--- src/com/arjankleene/weather_station/MainActivity.java
+++ src/com/arjankleene/weather_station/MainActivity.java
@@ -3,13 +3,20 @@ package com.arjankleene.weather_station;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.Manifest;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.arjankleene.astro.SolarInfo;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends Activity {
 
@@ -18,12 +25,10 @@ public class MainActivity extends Activity {
         @Override
         public void onLocationChanged(Location location) {
             if (location.hasAccuracy() && location.getAccuracy() < 100) {
-                locationManager.removeUpdates(locationListener);
+                stopLocationUpdates();
                 Toast.makeText(getBaseContext(), "Found accurate location", Toast.LENGTH_LONG).show();
+                displayTimes(location);
             }
-            String msg = "New Latitude: "+location.getLatitude()+"New Longitude: "+location.getLongitude();
-            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
-            updateLocationCounter();
         }
 
         @Override
@@ -46,8 +51,6 @@ public class MainActivity extends Activity {
         }
     };
 
-    private Integer seenLocations = -1;
-
     /**
      * Called when the activity is first created.
      */
@@ -60,36 +63,63 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
 
-        updateLocationCounter();
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        locationManager.removeUpdates(locationListener);
+        stopLocationUpdates();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        locationManager.removeUpdates(locationListener);
+        stopLocationUpdates();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    private void displayTimes(Location location) {
+        SolarInfo info = new SolarInfo(new GregorianCalendar(), location);
 
-        locationManager = null;
+        TextView sunriseTime = (TextView) findViewById(R.id.sunriseTime);
+        TextView sunsetTime = (TextView) findViewById(R.id.sunsetTime);
+
+        GregorianCalendar sunrise = info.getSunRise();
+        if (sunrise != null) {
+            sunriseTime.setText(formatTime(sunrise));
+        } else {
+            sunriseTime.setText("");
+        }
+
+        GregorianCalendar sunset = info.getSunSet();
+        if (sunset != null) {
+            sunsetTime.setText(formatTime(sunset));
+        } else {
+            sunsetTime.setText("");
+        }
     }
 
-    private void updateLocationCounter() {
-        TextView counter = (TextView) findViewById(R.id.counter);
-        seenLocations++;
-        counter.setText(seenLocations.toString());
+    private void stopLocationUpdates() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    private static String formatTime(GregorianCalendar calendar){
+        return formatTime(calendar, DateFormat.SHORT);
+    }
+
+    private static String formatTime(GregorianCalendar calendar, int style){
+        DateFormat fmt = SimpleDateFormat.getTimeInstance(style);
+        fmt.setCalendar(calendar);
+        String dateFormatted = fmt.format(calendar.getTime());
+        return dateFormatted;
     }
 }
